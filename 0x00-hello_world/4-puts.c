baff23f72e42c7241869e2427221cc555d64bc1a
Rhodaannie <annieassifuah8@gmail.com>
2023-04-20 12:18:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#iclude < studio.h >
+/**
+ * main - A program thats print a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
